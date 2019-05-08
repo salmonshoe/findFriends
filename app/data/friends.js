b2b3f67261ae0bfc116b5a3c446fab54606c87a3
salmonshoe <rafaelreynoso273@gmail.com>
2019-05-08 02:14:45 -0400

    Incomplete

--- app/data/friends.js
+++ app/data/friends.js
@@ -31,6 +31,22 @@ const peopleArray = [
             4
         ]
     },
+    {
+        name: "Bob the Builder",
+        photo: "https://m.media-amazon.com/images/M/MV5BNjRlYjgwMWMtNDFmMy00OWQ0LWFhMTMtNWE3MTU4ZjQ3MjgyXkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_CR0,45,480,270_AL_UX477_CR0,0,477,268_AL_.jpg",
+        scores: [
+        5,
+        2,
+        5,
+        1,
+        2,
+        5,
+        4,
+        2,
+        3,
+        5
+        ]
+        }
 
 ]
 module.exports = peopleArray;
